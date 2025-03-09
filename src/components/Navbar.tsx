83c535d238224ac0229104cdbdeca733530e2d11
Mohamed Tah <mohamed.tah901@gmail.com>
2025-03-09 15:00:29 +0100

    edit navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ import NavIcons from './NavIcons'
 
 const Navbar = () => {
   return (
-      <div className='h-20 px-4 md:px-16 lg:px-16 xl:px:16 2xl:px-16 bg-slate-200 relative' >
+      <div className='h-20 px-4 md:px-12 lg:px-12 xl:px:12 2xl:px-12 bg-slate-200 relative' >
         
         {/* Mobile */}
         <div className='md:hidden h-full flex justify-between items-center'>
@@ -19,24 +19,22 @@ const Navbar = () => {
         </div>
 
         {/* Bigger screens */}
-        <div className='hidden md:flex items-center justify-center gap-8 h-full'>
+        <div className='hidden md:flex items-center justify-center gap-4 h-full'>
             {/* Left */}
-            <div className="w-1/3 md:1/3 xl:w-1/2 flex items-center gap-12">
-                <Link href={"/"} className='flex items-center'>
+            <div className='w-2/3 flex justify-start'>
+                <Searchbar/>
+            </div>
+
+            {/* Center */}
+            <div className='w-1/3'>
+                <Link href={"/"} className='flex justify-center items-center'>
                 <Image src="/bg.png" alt='' width={120} height={70} className='p-2'/>
                 {/* <div className='text-2xl tracking-wide'>El Khaima</div> */}
                 </Link>
-                <div className="hidden xl:flex gap-4">
-                    <Link href="/">Homepage</Link>
-                    <Link href="/">Shop</Link>
-                    <Link href="/">Deals</Link>
-                    <Link href="/">About</Link>
-                    <Link href="/">Contact</Link>
-                </div>
             </div>
+
             {/* Right */}
-            <div className='w-2/3 flex justify-between items-center gap-8'>
-                <Searchbar/>
+            <div className='w-2/3 flex justify-end'>
                 <NavIcons/>
             </div>
         </div>
